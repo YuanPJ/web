2b06f49850d8c6bedccdf12055400bc0cb1630a8
YuanPJ <YuanPJ>
2017-03-10 04:00:56 +0800

    update sop

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    rules: {
+      'no-console': 0,
+      'no-else-return': 0,
+    },
+};
+
--- sop.sh
+++ sop.sh
@@ -2,4 +2,4 @@ npm init
 npm install --save-dev eslint
 npm install --save-dev eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y
 npm install --save-dev eslint-config-airbnb
-xcopy ../.eslintrc.js ./
+cp ../.eslintrc.js ./
