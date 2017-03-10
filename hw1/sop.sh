37996ac430e5bc5af0fb06c97977446fbfe6ade0
YuanPJ <YuanPJ>
2017-03-10 11:49:08 +0800

    ubuntu sop.sh

---
+++ hw1/sop.sh
@@ -0,0 +1,5 @@
+npm init
+npm install --save-dev eslint
+npm install --save-dev eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y
+npm install --save-dev eslint-config-airbnb
+cp ../.eslintrc.js ./
