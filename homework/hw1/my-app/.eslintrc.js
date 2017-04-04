9a1073daf9fcbc326d77d4c96975bf0000a86cd3
YuanPJ <YuanPJ>
2017-04-04 21:11:57 +0800

    Init

---
+++ homework/hw1/my-app/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: 'airbnb',
+    plugins: [
+        'react',
+        'jsx-a11y',
+        'import',
+    ],
+    rules: {
+      'no-console': 0,
+      'no-else-return': 0,
+    },
+    env: {
+        "browser": true
+    }
+};
