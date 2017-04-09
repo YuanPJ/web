2eab976fa8b2ae366cafa9cca8819896d8489c14
unknown <>
2017-04-10 03:18:50 +0800

    update sop with material-ui

--- sop.bat
+++ sop.bat
@@ -1,6 +1,8 @@
-create-react-app %1
-cd %1
-npm install --save-dev eslint
-npm install --save-dev eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y
-npm install --save-dev eslint-config-airbnb
-copy ..\.eslintrc.js .\
+call create-react-app %1
+call cd %1
+call npm install --save material-ui 
+call npm install --save react-tap-event-plugin
+call npm install --save-dev eslint
+call npm install --save-dev eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y
+call npm install --save-dev eslint-config-airbnb
+call copy ..\.eslintrc.js .\
--- sop.sh
+++ sop.sh
@@ -1,5 +1,7 @@
 creat-react-app $1
 cd $1
+npm install --save material-ui 
+npm install --save react-tap-event-plugin
 npm install --save-dev eslint
 npm install --save-dev eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y
 npm install --save-dev eslint-config-airbnb
