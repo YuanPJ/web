4b8795cb96fd485ff900bd5de6b41b049ab8d0cd
YuanPJ <YuanPJ>
2017-04-10 22:38:49 +0800

    Enter key and create Snackbar

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from '../node_modules/material-ui/styles/MuiThemeProvider';
 import TodoApp from './js/TodoApp';
 import './index.css';
 
+const App = () => (
+  <MuiThemeProvider>
+    <TodoApp />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <TodoApp />,
+  <App />,
   document.getElementById('root'),
 );
--- todo-app/src/js/TodoApp.js
+++ todo-app/src/js/TodoApp.js
@@ -1,15 +1,76 @@
 import React, { Component } from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import TextField from 'material-ui/TextField';
+import Snackbar from 'material-ui/Snackbar';
 import '../css/TodoApp.css';
 import Todolist from './TodoList';
 
+injectTapEventPlugin();
+
 class TodoApp extends Component {
   constructor() {
-    super('foo');
+    super();
+    this.state = {
+      lists: [],
+      newListName: '',
+      openMessage: false,
+      message: '',
+    };
+    this.handleCreateList = this.handleCreateList.bind(this);
+    this.handleChangeListName = this.handleChangeListName.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
   }
+
+  handleCreateList(e) {
+    let lists = this.state.lists;
+    const n = this.state.newListName;
+    if (e.key === 'Enter' && n !== '') {
+      const newList = {
+        listName: n,
+        todoItem: [],
+      };
+      lists = lists.concat(newList);
+      this.setState({
+        lists,
+        newListName: '',
+        openMessage: true,
+        message: `${n} todo-list created`,
+      });
+    }
+  }
+
+  handleChangeListName(e) {
+    const value = e.target.value;
+    this.setState({
+      newListName: value,
+    });
+  }
+
+  handleRequestClose() {
+    this.setState({
+      openMessage: false,
+      message: '',
+    });
+  }
+
   render() {
     return (
       <div className="TodoApp">
-        <h1>QQQQQ</h1>
+        <div>
+          <h1>TODO</h1>
+        </div>
+        <TextField
+          hintText="New List"
+          value={this.state.newListName}
+          onChange={this.handleChangeListName}
+          onKeyUp={this.handleCreateList}
+        />
+        <Snackbar
+          open={this.state.openMessage}
+          message={this.state.message}
+          autoHideDuration={1500}
+          onRequestClose={this.handleRequestClose}
+        />
       </div>
     );
   }
