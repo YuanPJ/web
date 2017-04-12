d73dc3a09d5ead1d79a5d29319ce2730914ed8af
YuanPJ <YuanPJ>
2017-04-12 21:56:14 +0800

    All function complete. too many states

---
+++ inclass/0412/sop.sh
@@ -0,0 +1,8 @@
+create-react-app $1
+cd $1
+npm install --save material-ui 
+npm install --save react-tap-event-plugin
+npm install --save-dev eslint
+npm install --save-dev eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y
+npm install --save-dev eslint-config-airbnb
+cp ../.eslintrc.js ./
